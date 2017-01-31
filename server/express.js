124a92742239d89f6cc954ddc13196280f9518f1
Dillon <dmcguire@emmisdigital.com>
2017-01-31 12:22:57 -0600

    mongo set up

--- server/express.js
+++ server/express.js
@@ -1,4 +1,28 @@
-var server = require('./lib/server-setup')();
+
+var mongoose    = require('mongoose'),
+    server      = require('./lib/server-setup')(),
+    dbname      = 'reacttodolist',
+    MONGOURI    = process.env.MONGOLAB_URI || 'mongodb://localhost:27017',
+    Schema      = mongoose.Schema;
+    PORT        = process.env.PORT || 3000;
+
+var todoSchema = new Schema({
+  title: { type: String, requiered: true, unique: false},
+  description: {type: String, requiered: false, unique: false},
+  isCompleted: {type: Boolean, default: false}
+})
+
+var Todo = mongoose.model('todo', todoSchema)
+mongoose.connect(MONGOURI + '/' + dbname)
+mongoose.set('debug', true)
+
+
+server.get('/todos', function(req, res) {
+  Todo.find({}, function(err, allTodos) {
+    res.send(allTodos)
+  })
+})
+
 
 server.listen(3000, function () {
   console.log('Server runninggggggggggggggg on 3000');
